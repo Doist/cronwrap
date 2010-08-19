0a34f849308aaecc6a2bdf5427ee722936448974
amix the lucky stiff <amix@amix.dk>
2010-08-19 17:21:58 +0200

    first commit

---
+++ README.markdown
@@ -0,0 +1,46 @@
+cronwrap
+===========================================
+
+A cron job wrapper that wraps jobs and enables better error reproting and command timeouts.
+
+Example
+===========
+
+Basic example of usage::
+
+    ##Will print out help
+    $ cronwrap -h
+
+        usage: cronwrap [-h] [-c CMD] [-e EMAILS] [-t TIME] [-v [VERBOSE]]
+
+        A cron job wrapper that wraps jobs and enables better error reproting and command timeouts.
+
+        optional arguments:
+          -h, --help            show this help message and exit
+          -c CMD, --cmd CMD     Run a command. Could be `cronwrap -c "ls -la"`.
+          -e EMAILS, --emails EMAILS
+                                Email following users if the command crashes or
+                                exceeds timeout. Could be `cronwrap -e
+                                "johndoe@mail.com, marcy@mail.com"`. Uses system's
+                                `mail` to send emails. If no command (cmd) is set a
+                                test email is sent.
+          -t TIME, --time TIME  Set the maxium running time.If this time is passed an
+                                alert email will be sent.The command will keep running
+                                even if maxium running time is exceeded.The default is
+                                1 hour `-t 1h`. Possible values include: `-t 2h`,`-t
+                                2m`, `-t 30s`.
+          -v [VERBOSE], --verbose [VERBOSE]
+                                Will send an email / print to stdout on succefull run.
+
+
+    ##Will send out a timeout alert to cron@my_domain.com:
+    $ cronwrap -c "sleep 2" -t "1s" -e cron@my_domain.com
+
+    ##Will send out an error alert to cron@my_domain.com:
+    $ cronwrap -c "blah" -e cron@my_domain.com
+
+    #Will not send any reports:
+    $ cronwrap -c "ls" -e cron@my_domain.com
+
+    #Will send a succefull report to cron@my_domain.com:
+    $ cronwrap -c "ls" -e cron@my_domain.com -v
---
+++ setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# Copyright (c) 2007 Qtrac Ltd. All rights reserved.
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+
+import os
+from setuptools import setup
+
+setup(name='cronwrap',
+      version = '1.1',
+      author="amix the lucky stiff",
+      author_email="amix@amix.dk",
+      url="http://www.amix.dk/",
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+
+      install_requires=[
+          'argparse>=1.1'
+      ],
+
+      scripts=['scripts/cronwrap'],
+      packages=[''],
+      platforms=["Any"],
+      license="BSD",
+      keywords='cron wrapper crontab cronjob',
+      description="A cron job wrapper that wraps jobs and enables better error reproting and command timeouts.",
+      long_description="""\
+Example
+-------
+
+Basic example of usage::
+
+    ##Will print out help
+    $ cronwrap -h
+
+        usage: cronwrap [-h] [-c CMD] [-e EMAILS] [-t TIME] [-v [VERBOSE]]
+
+        A cron job wrapper that wraps jobs and enables better error reproting and command timeouts.
+
+        optional arguments:
+          -h, --help            show this help message and exit
+          -c CMD, --cmd CMD     Run a command. Could be `cronwrap -c "ls -la"`.
+          -e EMAILS, --emails EMAILS
+                                Email following users if the command crashes or
+                                exceeds timeout. Could be `cronwrap -e
+                                "johndoe@mail.com, marcy@mail.com"`. Uses system's
+                                `mail` to send emails. If no command (cmd) is set a
+                                test email is sent.
+          -t TIME, --time TIME  Set the maxium running time.If this time is passed an
+                                alert email will be sent.The command will keep running
+                                even if maxium running time is exceeded.The default is
+                                1 hour `-t 1h`. Possible values include: `-t 2h`,`-t
+                                2m`, `-t 30s`.
+          -v [VERBOSE], --verbose [VERBOSE]
+                                Will send an email / print to stdout on succefull run.
+
+
+    ##Will send out a timeout alert to cron@my_domain.com:
+    $ cronwrap -c "sleep 2" -t "1s" -e cron@my_domain.com
+
+    ##Will send out an error alert to cron@my_domain.com:
+    $ cronwrap -c "blah" -e cron@my_domain.com
+
+    #Will not send any reports:
+    $ cronwrap -c "ls" -e cron@my_domain.com
+
+    #Will send a succefull report to cron@my_domain.com:
+    $ cronwrap -c "ls" -e cron@my_domain.com -v
+""")
